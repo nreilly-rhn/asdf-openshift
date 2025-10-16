0bfd5756979b9fea60c6bca5fb71a2ebf1b1dd40
Nathan Reilly <nreilly@redhat.com>
2025-10-16 18:14:16 +1000

    .

--- lib/utils.bash
+++ lib/utils.bash
@@ -17,3 +17,29 @@ list_all_versions() {
   	| sed ':a; N; $!ba; s/\n/ /g'
   printf "${versions}"
 }
+
+install_version() {
+  declare -A tools=([opensift-client]=oc [openshift-install]=openshift-install [oc-mirror]=oc-mirror)
+  for key in "${!languages[@]}"; do echo "$key"; done
+	#local install_type="$1"
+	#local version="$2"
+	#local install_path="${3%/bin}/bin"
+#
+	#if [ "$install_type" != "version" ]; then
+	#	fail "asdf-$TOOL_NAME supports release installs only"
+	#fi
+#
+	#(
+	#	mkdir -p "$install_path"
+	#	cp -r "$ASDF_DOWNLOAD_PATH"/promtool "$install_path"
+#
+	#	local tool_cmd
+	#	tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+	#	test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+#
+	#	echo "$TOOL_NAME $version installation was successful!"
+	#) || (
+	#	rm -rf "$install_path"
+	#	fail "An error occurred while installing $TOOL_NAME $version."
+	#)
+}
